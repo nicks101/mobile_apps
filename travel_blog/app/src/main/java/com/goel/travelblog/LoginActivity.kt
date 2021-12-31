840748be452b6b6c176e361a527fdffd83c8be80
Nikki Goel <gupta.nikkigupta@gmail.com>
2021-12-31 17:55:00 +0530

    login screen completed

--- travel_blog/app/src/main/java/com/goel/travelblog/LoginActivity.kt
+++ travel_blog/app/src/main/java/com/goel/travelblog/LoginActivity.kt
@@ -1,14 +1,86 @@
 package com.goel.travelblog
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import com.goel.travelblog.databinding.ActivityLoginBinding
 import android.os.Bundle
+import android.os.Handler
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+import com.google.android.material.textfield.TextInputLayout
+
+private lateinit var binding: ActivityLoginBinding // 1
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = ActivityLoginBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.loginButton.setOnClickListener { onLoginClicked() }
+
+        binding.textUsernameLayout.editText?.addTextChangedListener(createTextWatcher(binding.textUsernameLayout))
+        binding.textPasswordInput.editText?.addTextChangedListener(createTextWatcher(binding.textPasswordInput))
+    }
+
+    private fun createTextWatcher(textInput: TextInputLayout): TextWatcher {
+        return object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                textInput.error = null
+            }
+
+            override fun afterTextChanged(p0: Editable?) {}
+
+        }
+    }
+
+    private fun onLoginClicked() {
+        val username: String = binding.textUsernameLayout.editText?.text.toString()
+        val password: String = binding.textPasswordInput.editText?.text.toString()
+        if (username.isEmpty()) {
+            binding.textUsernameLayout.error = "Username must not be empty"
+        } else if (password.isEmpty()) {
+            binding.textPasswordInput.error = "Password must not be empty"
+        }
+        // TODO: This condition is not working as expected.
+        // Reproduce error: Put username as "admin" and anything in password other than "admin".
+        // Expected: Below condition will be true and error dialog would appear.
+        // Actual: The else block is run and performLogin function executes.
+        else if (username != "admin" && password != "admin") {
+            showErrorDialog()
+        } else {
+            performLogin()
+        }
+    }
+
+    private fun performLogin() {
+        binding.textUsernameLayout.isEnabled = false
+        binding.textPasswordInput.isEnabled = false
+        binding.loginButton.visibility = View.INVISIBLE
+        binding.progressBar.visibility = View.VISIBLE
+
+        Handler().postDelayed({
+            startMainActivity()
+            finish()
+        }, 2000)
+    }
+
+    private fun startMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun showErrorDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("Login Failed")
+            .setMessage("Username or password is not correct. Please try again.")
+            .setPositiveButton("OK") { dialog, _ -> dialog.dismiss() }
+            .show()
     }
 }
