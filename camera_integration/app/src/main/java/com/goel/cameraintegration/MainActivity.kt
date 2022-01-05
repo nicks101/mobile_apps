6c97ae176e929245d06448f6681aa75fb73ca500
Nikki Goel <gupta.nikkigupta@gmail.com>
2022-01-05 20:47:26 +0530

    android camera implemented using bitmap image

--- camera_integration/app/src/main/java/com/goel/cameraintegration/MainActivity.kt
+++ camera_integration/app/src/main/java/com/goel/cameraintegration/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.goel.cameraintegration
 
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
+import com.goel.cameraintegration.databinding.ActivityMainBinding
+
+private const val REQUEST_CODE = 42
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnTakePicture.setOnClickListener {
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+            try {
+                startActivityForResult(takePictureIntent, REQUEST_CODE)
+            } catch (e: ActivityNotFoundException) {
+                Log.i(TAG, e.toString())
+                Toast.makeText(this, "Unable to open camera", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            val takenImage = data?.extras?.get("data") as Bitmap
+            binding.imageView.setImageBitmap(takenImage)
+        } else {
+            Log.i(TAG, resultCode.toString())
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+
     }
 }
