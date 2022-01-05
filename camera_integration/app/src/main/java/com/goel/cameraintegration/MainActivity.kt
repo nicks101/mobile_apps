7e93eb59a903057e20f8d7ef086beaa698e83913
Nikki Goel <gupta.nikkigupta@gmail.com>
2022-01-05 23:15:37 +0530

    using fileprovider to capture and display image for better quality

--- camera_integration/app/src/main/java/com/goel/cameraintegration/MainActivity.kt
+++ camera_integration/app/src/main/java/com/goel/cameraintegration/MainActivity.kt
@@ -4,18 +4,24 @@ import android.app.Activity
 import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
+import androidx.core.content.FileProvider
 import com.goel.cameraintegration.databinding.ActivityMainBinding
+import java.io.File
 
 private const val REQUEST_CODE = 42
+private const val FILE_NAME = "photo.jpg"
 private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var photoFile: File
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +31,14 @@ class MainActivity : AppCompatActivity() {
 
         binding.btnTakePicture.setOnClickListener {
             val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            photoFile = getPhotoFile(FILE_NAME)
+
+            val fileProvider = FileProvider.getUriForFile(
+                this,
+                "com.goel.cameraintegration.fileprovider",
+                photoFile
+            )
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider)
 
             try {
                 startActivityForResult(takePictureIntent, REQUEST_CODE)
@@ -36,9 +50,14 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    private fun getPhotoFile(fileName: String): File {
+        val storageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(fileName, ".jpg", storageDirectory)
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-            val takenImage = data?.extras?.get("data") as Bitmap
+            val takenImage = BitmapFactory.decodeFile(photoFile.absolutePath)
             binding.imageView.setImageBitmap(takenImage)
         } else {
             Log.i(TAG, resultCode.toString())
